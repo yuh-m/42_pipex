4675aa507b20862f56035ef08f871163bac6f073
eryudi-m <eryudi-m@student.42sp.org.br>
2022-11-21 00:28:58 -0300

    backup

---
+++ pipex/inc/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 09:03:57 by eryudi-m          #+#    #+#             */
+/*   Updated: 2022/11/21 00:27:54 by eryudi-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+pipex(int argc, char **argv);
+
+#endif
